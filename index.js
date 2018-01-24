efbcd7252beff24e4a5989d5ed85f44e7d8a62a3
heathercoraje <yhk7830@hotmail.com>
2018-01-24 18:37:10 +0900

    diry code but working

--- index.js
+++ index.js
@@ -1,10 +1,25 @@
 (function allInOne() {
-  var searchTerm;
+  var inputForm = document.getElementById('searchTerm');
   var searchButton = document.getElementById('searchButton');
 
-  searchButton.addEventListener('click', function(){
+  inputForm.addEventListener('input', function () {
+    inputForm.style = "border: 0.15em solid #295e89;"
+    var inputSize = inputForm.value.length;
+    if (12 < inputSize ) {
+      inputForm.size = inputSize;
+    }
+    return
+  });
+
+  inputForm.addEventListener('onkeydown', function() {
+    if (event.keyCode==13) {
+      searchClick();
+    }
+    return
+  });
+
+  searchButton.addEventListener('click', function () {
     searchClick(searchTerm);
-    console.log('click!!');
   });
 
   function searchClick(searchTerm){
@@ -13,37 +28,26 @@
     var output = document.getElementById('output');
     var url ="https://en.wikipedia.org/w/api.php?origin=*&action=opensearch&search=" + searchTerm;
 
+    output.innerHTML = "";
     fetch(url, {
       method: 'GET'
     }).then(function (response) {
       return response.json();
     }).then(function (myJson) {
-      output.innerHTML = "";
-      for (var i = 0; i < myJson[1].length; i++) {
-        var eachOutput = document.createElement('div');
-        var link = document.createElement('a');
-        link.innerHTML = myJson[1][i];
-        link.href = myJson[3][i];
-        eachOutput.appendChild(link);
-        var desc = document.createElement('li');
-        desc.innerHTML = myJson[2][i];
-        eachOutput.appendChild(desc);
-        output.appendChild(eachOutput);
-      };
-      console.log(output);
+        for (var i = 0; i < myJson[1].length; i++) {
+          var eachOutput = document.createElement('div');
+          eachOutput.className = 'eachOutput'
+          var link = document.createElement('a');
+          link.innerHTML = myJson[1][i];
+          link.href = myJson[3][i];
+          eachOutput.appendChild(link);
+          var desc = document.createElement('li');
+          desc.innerHTML = myJson[2][i];
+          eachOutput.appendChild(desc);
+          output.appendChild(eachOutput);
+        };
     }).catch(function (error) {
       console.log(error);
     });
   }
 })();
-
-
-var inputForm = document.getElementById('searchTerm');
-
-inputForm.addEventListener('input', function() {
-  var inputSize = inputForm.value.length;
-  if (12 < inputSize ) {
-    inputForm.size = inputSize;
-  }
-  return
-});
